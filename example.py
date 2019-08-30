18ca0a9c72cb38458afc45287897b9147c9d0426
Taiiwo Llort <Taiiwo@Llort.gq>
2019-08-30 01:42:09 +0100

    More example code that showcases the gematria library

--- example.py
+++ example.py
@@ -1,7 +1,13 @@
 from cicada import LiberPrimus
+from cicada.gematria import Latin, Runes
 
 lp = LiberPrimus()
 
-for page in lp.pages:
+shift = 0
+for page in lp.pages[0:3]:
     page.substitute("ᚠᚢᚦᚩᚱᚳᚷᚹᚻᚾᛁᛄᛇᛈᛉᛋᛏᛒᛖᛗᛚᛝᛟᛞᚪᚫᚣᛡᛠ", "ᛠᛡᚣᚫᚪᛞᛟᛝᛚᛗᛖᛒᛏᛋᛉᛈᛇᛄᛁᚾᚻᚹᚷᚳᚱᚩᚦᚢᚠ")
+    page.shift(shift)
     print(page.to_latin())
+
+print(Runes("ᚠᚢᚦᚩᚱᚳᚷᚹᚻᚾᛁᛄᛇᛈᛉᛋᛏᛒᛖᛗᛚᛝᛟᛞᚪᚫᚣᛡᛠ").to_latin())
+print(Latin("Hello World!").to_runes())
