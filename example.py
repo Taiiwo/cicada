97aef2a86b5a22441356bfb0de7abf50d57ac02e
Taiiwo Llort <Taiiwo@Llort.gq>
2020-11-30 04:34:03 +0000

    Add a more complex example

--- example.py
+++ example.py
@@ -4,31 +4,8 @@ from cicada.gematria import Latin, Runes
 # load the lp from file
 lp = LiberPrimus()
 
-"""
-shift = 0
-# for pages 0 - 3 of the lp
-for page in lp.pages[0:3]:
-    # page is a Runes() object so we can do some cool things:
-    # atbash substitution
-    page.substitute("ᚠᚢᚦᚩᚱᚳᚷᚹᚻᚾᛁᛄᛇᛈᛉᛋᛏᛒᛖᛗᛚᛝᛟᛞᚪᚫᚣᛡᛠ", "ᛠᛡᚣᚫᚪᛞᛟᛝᛚᛗᛖᛒᛏᛋᛉᛈᛇᛄᛁᚾᚻᚹᚷᚳᚱᚩᚦᚢᚠ")
-    # caesar shift
-    page.shift(shift)
-    # use .text to get the contents of a Gematria() object such as Runes()
-    # Note: it will also convert to str with str(Runes()) or equiv
-    print("Runes: %s" % page.text)
-    print("Plaintext: %s" % page.to_latin())
-    # example of automated shifting
-    shift += 1
-
-print(Runes("ᚠᚢᚦᚩᚱᚳᚷᚹᚻᚾᛁᛄᛇᛈᛉᛋᛏᛒᛖᛗᛚᛝᛟᛞᚪᚫᚣᛡᛠ").to_latin())
-# FUTHORKGWHNIJEOPXZTBEMLINGOEDAAEYIAEA
-print(Latin("Hello World!").to_runes())
-# ᚻᛖᛚᛚᚩ ᚹᚩᚱᛚᛞ!
-print(Latin("How would cicada type this?").to_runes().to_latin())
-# HOW WOULD KIKADA TYPE THIZ?
-"""
 cipher = Latin("crack me, baby. This is some text to crack. Let's see if we can crack it")
-cipher.substitute("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "QWERTYUIOPASDFGHJKLZXCVBNM")
+cipher.substitute("ABCDEFGHIJKLMN", "QWERTYUIOPASDF")
 #cipher.substitute("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNOPQRSUTVWXYZ")
 print(cipher)
 cipher = cipher.to_runes().text
