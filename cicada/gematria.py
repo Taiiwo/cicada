77530a274ccf674e54a99b0aaf300f3e0d2e13dc
Taiiwo Llort <Taiiwo@Llort.gq>
2021-03-21 20:10:26 +0000

    Allow interrupts in lower case and as a string

--- cicada/gematria.py
+++ cicada/gematria.py
@@ -127,14 +127,16 @@ class Cipher:
     def gematria_sum_lines(self):
         return [Runes(w).gematria_sum() for w in self.text.splitlines()]
 
-    def running_shift(self, key, interrupts=[]):
+    def running_shift(self, key, interrupts=""):
+        if type(interrupts) == list:
+            interrupts = "".join(interrupts)
         o = ""
         i = 0
 
         for c in self.text:
             if c not in self.alpha:
                 continue
-            if c in interrupts:
+            if c in interrupts.upper():
                 o += c
                 continue
             c_index = self.alpha.index(c)
