f0dab63cc092e4060f9cb634f396d169f87bacd0
Taiiwo <Taiiwo@Llort.gq>
2025-06-14 22:22:07 +0100

    improve example

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ from cicada.gematria import Latin, Runes
 lp = LiberPrimus()
 
 cipher = Latin("crack me, baby. This is some text to crack. Let's see if we can crack it")
-cipher.sub("ABCDEFGHIJKLMN", "QWERTYUIOPASDF")
+cipher = cipher.sub("ABCDEFGHIJKLMN", "QWERTYUIOPASDF")
 #cipher.substitute("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "ABCDEFGHIJKLMNOPQRSUTVWXYZ")
 print(cipher)
 cipher = cipher.to_runes().text
@@ -23,9 +23,10 @@ cipher = Runes(cipher).to_latin()
 # Try every combination of substitutes
 from cicada.pybar import PyBar
 import math
-bar = PyBar(max=math.factorial(19), poll=1)
-for combination in permutations("YRABMSTHFLNETCXIOWK"):
-    text = cipher.sub("YRABMSTHFLNETCXIOWK", "".join(combination))
-    bar.update(bar.percent(), bar.progress(), bar.bar(), bar.rate(time=1), "Current: ", text.text, next=True)
-    if v.is_cicadian(text.text):
-        bar.echo(text)
+bar = PyBar(max=math.factorial(8), poll=1)
+for combination in permutations("ABCDEFGH"):
+    key = "".join(combination)
+    text = cipher.sub("ABCDEFGH", key)
+    bar.update(bar.percent(), bar.progress(), bar.bar(), bar.rate(time=1), "Current: "+key , next=True)
+    if v.is_cicadian(text.text, 40) or key == "GABCFHDE":
+        bar.echo(text.text + "WITH KEY: " + "".join(combination))
